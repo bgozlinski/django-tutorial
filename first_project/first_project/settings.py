9d887d76f6e34b1252c23d441c32f7ae75ed05a3
bgozlinski <BARTLOMIEJ.GOZLINSKI@GMAIL.COM>
2025-06-12 02:49:59 +0200

    added login site

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -127,3 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'blog-home'
+
