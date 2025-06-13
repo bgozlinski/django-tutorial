a064c972ecbce4cde7bfdf835a949fdb7d7af1a2
bgozlinski <BARTLOMIEJ.GOZLINSKI@GMAIL.COM>
2025-06-13 10:45:56 +0200

    Added CRUD for Articles.

--- first_project/blog/views.py
+++ first_project/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Article
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 def home(request):
     return render(request, "blog/home.html", {"title": "Home", "posts": Article.objects.all()})
@@ -7,3 +11,49 @@ def home(request):
 
 def about(request):
     return render(request, "blog/about.html", {"title": "About"})
+
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'blog/article.html'
+
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = Article
+    template_name = 'blog/article_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Article
+    template_name = 'blog/article_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        article = self.get_object()
+        return article.author == self.request.user
+
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Article
+    template_name = 'blog/delete_confirm.html'
+    success_url = '/'
+
+    def test_func(self):
+        article = self.get_object()
+        return article.author == self.request.user
